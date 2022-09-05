6d9b9571afed9fcd6dc7bf280cb6e9bb628a311b
박종우 <indra@allm.co.kr>
2022-09-05 11:39:57 +0900

    frontend

---
+++ frontend-react/todo/src/SignUp.js
@@ -0,0 +1,96 @@
+import React from 'react';
+import { signup } from './service/ApiService';
+import {
+  Button,
+  TextField,
+  Link,
+  Grid,
+  Typography,
+  Container,
+} from '@material-ui/core';
+
+class SignUp extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+    const data = new FormData(event.target);
+    const email = data.get('email');
+    const password = data.get('password');
+    const username = data.get('username');
+    signup({ email: email, password: password, username: username });
+  }
+
+  render() {
+    return (
+      <Container component="main" maxWidth="xs" style={{ margintop: '8%' }}>
+        <Grid container spacing={2}>
+          <Grid item xs={12}>
+            <Typography component="h1" variant="h5">
+              계정 생성
+            </Typography>
+          </Grid>
+        </Grid>
+        <form noValidate onSubmit={this.handleSubmit}>
+          <Grid container spacing={2}>
+            <Grid item xs={12}>
+              <TextField
+                variant="outlined"
+                required
+                fullWidth
+                id="username"
+                label="사용자 이름"
+                name="username"
+                autoFocus
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <TextField
+                variant="outlined"
+                required
+                fullWidth
+                id="email"
+                label="이메일 주소"
+                name="email"
+                autoComplete="email"
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <TextField
+                variant="outlined"
+                required
+                fullWidth
+                id="password"
+                label="패스워드"
+                name="password"
+                autoComplete="current-password"
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <Button
+                type="submit"
+                fullWidth
+                variant="contained"
+                color="primary"
+              >
+                계정 생성
+              </Button>
+            </Grid>
+          </Grid>
+          <Grid container justifyContent="flex-end">
+            <Grid item>
+              <Link href="/login" variant="body2">
+                이미 계정이 있습니까? 로그인 하세요.
+              </Link>
+            </Grid>
+          </Grid>
+        </form>
+      </Container>
+    );
+  }
+}
+
+export default SignUp;
---
+++ frontend-react/todo/src/service/ApiService.js
@@ -0,0 +1,60 @@
+import { API_BASE_URL } from '../app-config';
+const ACCESS_TOKEN = 'ACCESS_TOKEN';
+
+export function call(api, method, request) {
+  let headers = new Headers({ 'Content-Type': 'application/json' });
+
+  const accessToken = localStorage.getItem(ACCESS_TOKEN);
+  if (accessToken && accessToken !== null) {
+    headers.append('Authorization', 'Bearer ' + accessToken);
+  }
+
+  let options = {
+    headers: headers,
+    url: API_BASE_URL + api,
+    method: method,
+  };
+  if (request) {
+    options.body = JSON.stringify(request);
+  }
+  return fetch(options.url, options)
+    .then((response) => {
+      if (response.ok) {
+        return response.json();
+      }
+      return Promise.reject(response.status);
+    })
+    .catch((error) => {
+      console.log(error);
+      if (error === 403) {
+        window.location.href = '/login';
+      }
+      return Promise.reject(error);
+    });
+}
+
+export async function signin(userDTO) {
+  try {
+    const response = await call('/auth/signin', 'POST', userDTO);
+    if (response.token) {
+      localStorage.setItem(ACCESS_TOKEN, response.token);
+      window.location.href = '/';
+    }
+  } catch {
+    alert('로그인에 실패하였습니다.');
+  }
+}
+
+export async function signout() {
+  localStorage.removeItem(ACCESS_TOKEN);
+  window.location.href = '/login';
+}
+
+export async function signup(userDTO) {
+  try {
+    const response = await call('/auth/signup', 'POST', userDTO);
+    window.location.href = '/login';
+  } catch (e) {
+    alert('이미 가입한 이메일 입니다.');
+  }
+}
